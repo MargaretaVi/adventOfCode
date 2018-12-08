a50c9ecf3df8e3ed07c0f1bfc7e00548addc1e59
Maggie <margareta.vi@gmail.com>
2018-12-08 23:23:41 +0100

    woking in worgess for day6

--- 2018/day6/day6.cpp
+++ 2018/day6/day6.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 #include <algorithm>
 #include <limits.h>
-#include <set>
-
+#include <map>
+#include <typeinfo>
 
 using namespace std;
 
 //Split line on blankspace " "
-void splitLine(string s, vector<int> &xValues, vector<int> &yValues) {
+void splitLine(string s, vector<int> &xValues, vector<int> &yValues, vector<pair<int,int>> &inputCoordinates) {
     size_t pos = 0;
     std::string x;
     std::string y;
@@ -24,6 +24,7 @@ void splitLine(string s, vector<int> &xValues, vector<int> &yValues) {
 
         s.erase(0, pos + delimiter.length());
     }
+    inputCoordinates.push_back(make_pair(stoi(x),stoi(y)));
 }
 
 int calculateMHDist(int myX, int myY, int boardX, int boardY) {
@@ -33,20 +34,97 @@ int calculateMHDist(int myX, int myY, int boardX, int boardY) {
 int main () {
     
     string _line;
-    vector<pair<int,int>> dataLines;
+    vector<pair<int,int>> inputCoordinates;
     vector<int> xValues, yValues;
+    map<pair<int,int>, map<int,int>> board;
+    ifstream myfile("test.txt");
 
-    ifstream myfile("init.txt");
-
-    if (myfile.is_open()) {
+     if (myfile.is_open()) {
         while (getline (myfile,_line)) {
             //remove ","
             _line.erase(remove(_line.begin(), _line.end(), ','), _line.end());
-            splitLine(_line, xValues, yValues);
+            splitLine(_line, xValues, yValues, inputCoordinates);
         }
         myfile.close();
     }
     else cout << "Unable to open file";
 
+    // finding the max of x and y
+    vector<int>::iterator tmp;
+    tmp = max_element(xValues.begin(), xValues.end());
+    int xMax = xValues[distance(xValues.begin(), tmp)];
 
+    tmp = max_element(yValues.begin(), yValues.end());
+    int yMax = yValues[distance(yValues.begin(), tmp)];
+    int counter = 1;
+    int x,y;
+    cout << "max values: " << xMax << " " << yMax << endl; 
+    // set up all the values for the board.
+        for (x = 0; x <= xMax ; x++) {
+            for (y = 0; y <= yMax ; y++) {
+                counter = 1;
+                for (auto p: inputCoordinates) {
+                    int tmpXValue = p.first;
+                    int tmpYValue = p.second;
+                    int dist = INT_MAX; 
+                    // not on the boarder
+                    if (!(x == 0 || y == 0 || x == xMax || y == yMax ||tmpXValue == xMax || tmpYValue == yMax)) 
+                    {
+                        dist = calculateMHDist(tmpXValue, tmpYValue, x, y);
+                      /*  if (tmpXValue == 1) {
+                            cout << " cords " << x << "," << y << "   the distance: " << dist << "   my values " << tmpXValue << "," << tmpYValue <<'\n';
+                        }
+
+                        if (dist == 0) {
+                            cout << " cords " << x << "," << y << "  mmy input" << tmpXValue << "," << tmpYValue << '\n';
+                        }*/
+                    }
+                    board[pair<int, int> (x,y)][counter] = dist;
+                    counter ++;
+                }
+            }
+        }
+       
+    map<int,int> scoreHolder; 
+    // for each coordinate on the board
+
+    for (auto kv : board) {
+        int tempWinner = INT_MAX;
+        int distanceToMe = INT_MAX;
+        // for each input coordiante
+        for (auto innerKv : kv.second) {
+            if (innerKv.second < distanceToMe) {
+                distanceToMe = innerKv.second;
+                tempWinner = innerKv.first;
+            }
+            //cout << " coodinate : " << kv.first.first << "," << kv.first.second << " current " << innerKv.second << "  who am i_ " << innerKv.first << endl; // " challenger" << innerKv.second << endl;
+
+            if (innerKv.second == distanceToMe) {
+                continue;
+                //tempWinner = INT_MAX;
+            }
+        }
+        if (tempWinner != INT_MAX && distanceToMe !=INT_MAX) {
+            if (scoreHolder.count(tempWinner)) {
+                cout << " coodinate : " << kv.first.first << "," << kv.first.second << "   winner " << tempWinner << " ditnace " << distanceToMe << endl;
+                scoreHolder.at(tempWinner) += 1;
+            }
+            else {
+                scoreHolder[tempWinner] = 1;
+            }
+        }    
+    }
+
+    int area = 0;
+    int winner = INT_MAX; 
+    cout << "the size of score holder is " << scoreHolder.size() << endl;
+    for (auto coord : scoreHolder) {
+        if (coord.second > area) {
+            area = coord.second;
+            winner = coord.first;
+        }
+    }
+
+    cout << "The alrgest area is : " << area << " and the coodinate winner is: " << winner << endl;
+    return 0;
 }
