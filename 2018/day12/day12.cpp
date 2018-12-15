0c4b3604c3f48333052208773f9b2bf8d69ac17d
Maggie <margareta.vi@gmail.com>
2018-12-15 20:46:03 +0100

    finished day 12

--- 2018/day12/day12.cpp
+++ 2018/day12/day12.cpp
@@ -1,17 +1,18 @@
-#include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <deque>
+#include <vector>
 
 using namespace std;
 
 // the indexing starts on -3
-int countPots(string potStr) {
-    int sum = 0;
+uint64_t countPots(string potStr, uint64_t offSet) {
+    uint64_t sum = 0;
     for (string::iterator iter = potStr.begin(); iter != potStr.end(); iter++) {
         if (*iter == '#') {
-            sum += (iter - potStr.begin()) - 3;
+            sum += (iter - potStr.begin()) + offSet;
         }
     }   
     return sum;
@@ -22,7 +23,7 @@ int main() {
 
     string _line;
     vector<string> dataLines;
-    ifstream myfile ("init.txt");
+    ifstream myfile("init.txt");
     if (myfile.is_open()) {
         while (getline (myfile,_line) ) {
             dataLines.push_back(_line);
@@ -34,13 +35,10 @@ int main() {
     }
 
     //split the data into init state and "instructions";
-    string initState (dataLines[0].begin() + 15, dataLines[0].end());
+    string initState = "#.#.#...#..##..###.##.#...#.##.#....#..#.#....##.#.##...###.#...#######.....##.###.####.#....#.#..##";
+    //initState = string(dataLines[0].begin() + 15, dataLines[0].end()); // cannot add padding to the end of this string
     vector<string> instructionsTmp (dataLines.begin() + 2, dataLines.end());
 
-    // the int should indicate the generation
-    vector<pair<int,string>> plantStates;
-    // add the inititalize state
-    plantStates.push_back(pair<int,string> (0,initState));
 
     vector<pair<string,string>> instructions;
     // should match string to string in the instructions
@@ -51,42 +49,53 @@ int main() {
         answer = ins.substr(equal + 3);
         instructions.push_back(pair<string,string> (match, answer));
     }
+    string tmpSmallString, newStateString;
+    string newString = initState;
+    uint64_t offSet = 0;
+    string tmp = "...." + newString + "....";
+    cout << 0 << "    " << tmp << endl;
+    for (auto gen= 1; gen <=3000; gen++) {
+        auto firstSharp = newString.find('#');
+        while (firstSharp < 4) {
+            newString.insert(0, ".");
+            offSet--;
+            firstSharp = newString.find('#');
+        }
 
+        auto lastSharp = newString.find_last_of('#');
+        while ((newString.size() - lastSharp) < 5) {
+            newString.insert(newString.end(), '.');
+            lastSharp = newString.find_last_of('#');
+        }
+
+        // new state string every loop
+        newStateString = string(newString.size(), '.');
+        string::iterator modifyIter = newStateString.begin();
+        for (string::iterator start = newString.begin(); start != newString.end()-5; start++) {
+            tmpSmallString = string(start, start + 5);
+            for (auto ins : instructions) {
+                if (tmpSmallString == ins.first) {
+                    const char tmp = *ins.second.c_str();
+                    *(modifyIter+2) = tmp;
+                }
+            }
+            modifyIter++;
+        }
+        newString = newStateString;
+        //cout << gen << ":   " << newStateString << endl; 
+    }
+    
     // "UT"
-    string testString = ".#....##....#####...#######....#.#..##.";
-    int test = countPots(testString);
-    if (test == 325) {
+    //string testString = ".#....##....#####...#######....#.#..##.";
+    uint64_t test = countPots(newStateString, offSet);
+    /*if (test == 325) {
         cout << "success! \n";
     } 
     else {
         cout << test << endl;
         cout << "You did wrong \n";
-    }
+    }*/
+    cout << "this is the answer: " << test << endl;
 
-    for (auto g = 1; g< 21; g++) {
-        string lastGen = plantStates.back().second;
-        string::iterator it = lastGen.begin();
-        vector<int> indices;
-        string valueToFind = "#";
-        size_t pos = lastGen.find(valueToFind);
-
-        while( pos != string::npos) {
-            indices.push_back(pos);
-            pos = lastGen.find(valueToFind, pos + valueToFind.size());
-        }
-
-        for (auto ind : indices) {
-            string tmp = "";
-            // fix so that we update the generation with the instructions...
-            //plantStates.push_back(pair<int, string> (ind, lastGen));
-        }
-    }
-
-
-    /*for (auto state : plantStates) {
-        cout << "Generation " << state.first << " : " << state.second << endl;
-    }
-    */
-
-    return 0;
+   return 0;
 }
